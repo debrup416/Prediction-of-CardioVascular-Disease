0aed66f3969c30cb6aed7dbfe430c2c1577580f4
debrup416 <debrup416@gmail.com>
2022-05-24 02:26:28 +0530

    Done

--- cardioVascular/settings.py
+++ cardioVascular/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2(m35+shl))cap8mq**y6rm8y@mf6a3!@=39cq@hnxig(hu1b8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
